2ca7e71db1d63ab57cf22703d955173ecea5ab62
David Caldwell <doubledc92@gmail.com>
2022-11-15 14:32:50 -0500

    getting image, species, info to display for animals

--- client/src/components/AnimalCard.jsx
+++ client/src/components/AnimalCard.jsx
@@ -8,7 +8,7 @@ const AnimalDisplay = ({onClick, animal, name, image, species, information}) => 
         <div className="card exhibit-card"onClick={() => onClick(animal)}  >
             <div className="info-wrapper flex-col">
                 <h3 className="runDateDisplay"> name: {name}</h3>
-                <img className="cardDisplay"src={image} />
+                <img className="cardDisplay" src={image} alt='animal' />
                 <h4 className="cardDisplay"> Species: {species} </h4>
                 <p className="cardDisplay">Information: {information}</p>
             </div>
