81074deaa7e99636af5f10852bdaef8b4a254b8b
Marley Judd <marleymaejudd@gmail.com>
2022-11-15 12:03:56 -0700

    render animal cards

---
+++ client/src/pages/ExhibitAnimals.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import { GetAnimalType } from '../services/AnimalsServices'
+import { useNavigate, useParams } from 'react-router-dom'
+import AnimalCard from '../components/AnimalCard'
+
+const ExhibitAnimals = () => {
+  let { id } = useParams()
+  let navigate = useNavigate()
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+    const handleAnimals = async () => {
+      const data = await GetAnimalType(id)
+      setAnimals(data)
+    }
+    handleAnimals()
+  }, [])
+
+  return (
+    <div>
+      {animals.map((animal) => (
+        <AnimalCard
+          key={animal.id}
+          name={animal.name}
+          image={animal.image}
+          // onClick={() => viewAnimals(exhibit.id)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ExhibitAnimals
--- client/src/services/AnimalsServices.js
+++ client/src/services/AnimalsServices.js
@@ -8,3 +8,21 @@ export const GetAnimals = async () => {
     throw error
   }
 }
+
+export const GetOneAnimal = async (id) => {
+  try {
+    const res = await Client.get(`/animal/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetAnimalType = async (id) => {
+  try {
+    const res = await Client.get(`/animal/type/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
